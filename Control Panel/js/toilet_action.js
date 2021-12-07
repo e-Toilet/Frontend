3ef6b86db71529d79576a59380f6944432d58d41
Ming <billkuojhgf@gmail.com>
2021-12-07 19:12:13 +0800

    first init

---
+++ Control Panel/js/toilet_action.js
@@ -0,0 +1,21 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        toilets: [
+            {toilet_id:"1", toilet_name:"第一廁所"},
+            {toilet_id:"2", toilet_name:"第二廁所"},
+            {toilet_id:"3", toilet_name:"第三廁所"},
+            {toilet_id:"4", toilet_name:"第四廁所"},
+            {toilet_id:"4", toilet_name:"第四廁所"},
+            {toilet_id:"4", toilet_name:"第四廁所"},
+            {toilet_id:"4", toilet_name:"第四廁所"},
+            {toilet_id:"4", toilet_name:"第四廁所"},
+            {toilet_id:"4", toilet_name:"第四廁所"}
+        ]
+    },
+    methods: {
+        test: (id) => {
+            alert(`Hello, ${id}`)
+        }
+    }
+})
