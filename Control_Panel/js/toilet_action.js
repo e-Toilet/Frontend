50721fa00cfd3df10d30cba660eb959e9232a24a
FHIR Proj <fhirproj@dynamic187.mgt.ncu.edu.tw>
2021-12-08 21:45:19 +0800

    Homepage was added into the project

--- Control_Panel/js/toilet_action.js
+++ Control_Panel/js/toilet_action.js
@@ -4,7 +4,6 @@ const app = new Vue({
         toilets: [
             {toilet_id:"1", toilet_name:"第一廁所"},
             {toilet_id:"2", toilet_name:"第二廁所"},
-            {toilet_id:"3", toilet_name:"第三廁所"},
             {toilet_id:"4", toilet_name:"第四廁所"},
             {toilet_id:"4", toilet_name:"第四廁所"},
             {toilet_id:"4", toilet_name:"第四廁所"},
