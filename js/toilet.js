b2d426878ceee6daeeef80502d7868fc950e4ccf
406402373 <50175524+406402373@users.noreply.github.com>
2021-12-16 20:30:47 +0800

    toilet.html 有增加顯示 地址欄位，js檔先改為getToiletByLoc?district

--- js/toilet.js
+++ js/toilet.js
@@ -4,7 +4,7 @@ const showtoilet = new Vue({
         toilets: []
     },
     created: () => {
-        axios.get('http://140.115.87.117:8090/getAllToilet')
+        axios.get('http://140.115.87.117:8090/getToiletByLoc?district_id=36')
         .then((response) => {
             result = JSON.parse(response.data.Toiletinfo)
             showtoilet.toilets = result
