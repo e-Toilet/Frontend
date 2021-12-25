9530b95893c77edf104f445e79817aff307d7557
406402373 <50175524+406402373@users.noreply.github.com>
2021-12-25 12:57:41 +0800

    會員資料還無法正確更新

--- js/vue/account_member_action.js
+++ js/vue/account_member_action.js
@@ -1,26 +1,41 @@
 const app = new Vue({
-  el: "#app",
-  data: {
-    account: {},
-    comment: 0,
-  },
-  created: () => {
-    axios
-      .get("http://140.115.87.117:8090/getMemberInfo?member_id=112")
-      .then((response) => {
-        result = JSON.parse(response.data.Memberinfo);
-        app.account = result[0];
-        //                console.log(result)
-      });
-    axios
-      .get("http://140.115.87.117:8090/getMemberReviewCount?member_id=112")
-      .then((response) => {
-        result = JSON.parse(response.data.Review_count);
-        app.comment = result;
-        console.log(result);
-      });
-  },
-  methods: {
-   
-  },
-});
+    el: "#app",
+    data: {
+        account: {},
+        account_info: {},
+        comment: 0,
+        password: "",
+        checkpass: "",
+    },
+    created: () => {
+        axios
+            .get("http://140.115.87.117:8090/getMemberInfo?member_id=112")
+            .then((response) => {
+                result = JSON.parse(response.data.Memberinfo);
+                app.account = result[0];
+                //                console.log(result)
+            });
+        axios
+            .get("http://140.115.87.117:8090/getMemberReviewCount?member_id=112")
+            .then((response) => {
+                result = JSON.parse(response.data.Review_count);
+                app.comment = result;
+                console.log(result);
+            });
+    },
+    methods: {
+        update_account: () => {
+            console.log(app.account_info)
+            axios
+                .post("http://140.115.87.117:8090/updateMemberInfo", app.account_info)
+                .then(() => {
+                    // app.account
+                    app.account[app.account_info.member_id - 1] = app.account_info;
+                })
+                .catch((error) => {
+                    console.log()
+                    alert("輸入錯誤")
+                });
+        }
+    }
+})
