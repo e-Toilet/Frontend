efdb3249eb96b756f3a16230633ef3d77bfd481e
406402373 <50175524+406402373@users.noreply.github.com>
2021-12-26 18:52:26 +0800

    修改會員資料好了

--- js/vue/account_member_action.js
+++ js/vue/account_member_action.js
@@ -2,7 +2,12 @@ const app = new Vue({
     el: "#app",
     data: {
         account: {},
-        account_info: {},
+        account_info: {
+            name: "",
+            email: "",
+            password: "",
+            member_id:112,
+        },
         comment: 0,
         password: "",
         checkpass: "",
@@ -13,6 +18,9 @@ const app = new Vue({
             .then((response) => {
                 result = JSON.parse(response.data.Memberinfo);
                 app.account = result[0];
+                app.account_info.name = app.account.name
+                app.account_info.email = app.account.email
+                app.account_info.password = app.account.password
                 //                console.log(result)
             });
         axios
@@ -26,16 +34,44 @@ const app = new Vue({
     methods: {
         update_account: () => {
             console.log(app.account_info)
-            axios
-                .post("http://140.115.87.117:8090/updateMemberInfo", app.account_info)
-                .then(() => {
-                    // app.account
-                    app.account[app.account_info.member_id - 1] = app.account_info;
-                })
-                .catch((error) => {
-                    console.log()
-                    alert("輸入錯誤")
-                });
-        }
+            let checkupdate=app.validEmail(app.account_info.email) && app.validPassword(app.account_info.password)
+            if (checkupdate) {
+                axios
+                    .post("http://140.115.87.117:8090/updateMemberInfo",
+                        app.account_info
+                    )
+                    .then(() => {
+                        swal({
+                            title: "修改成功",
+                            icon: "success",
+                        }).then(function () {
+                            self.location.reload();
+                        })
+                        // app.account
+                        app.account[app.account_info.member_id - 1] = app.account_info;
+                    })
+                    .catch((error) => {
+                        console.log()
+                        swal({
+                            title: "修改失敗",
+                            icon: "error",
+                        });
+                    });
+            }
+            if (!app.validEmail(app.account_info.email)) {
+                alert('請輸入有效的Email');
+            }
+            if (!app.validPassword(app.account_info.password)) {
+                alert('Password格式不符合，需要超過6位之字母或數字');
+            }
+        },
+        validEmail: function (email) {
+            var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return re.test(email);
+        },
+        validPassword: function (password) {
+            var re = /[A-Za-z0-9]{6,}/;
+            return re.test(password);
+        },
     }
 })
